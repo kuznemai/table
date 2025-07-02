70a383ecbf4aa4e7bbf840dc23f045b6483d0019
Marina Kuznetsova <marinakuznetsova420@gmail.com>
2025-07-02 21:50:39 +0300

    workflow CI/CD and fixed routing

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? env.VITE_BASE_URL : '/',
+  // base: process.env.NODE_ENV === 'production' ? env.VITE_BASE_URL : '/',
 });
